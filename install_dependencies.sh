cf1240a608e39f8665a3d3c548a098d9327e69d3
Oleguer <oleguer@mit.edu>
2018-04-11 16:42:50 +0200

    Stitching PointClouds (pcl c++)

---
+++ install_dependencies.sh
@@ -0,0 +1,7 @@
+# Install pypcd
+pip install pypcd
+
+# Install python-pcl
+sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl -y
+sudo apt-get update -y
+sudo apt-get install libpcl-all -y
