ce59b8e839af5ada03cdc80eaaae8396c01cc4ac
Oleguer <oleguer@mit.edu>
2018-04-12 23:38:37 +0200

    Minor bugs fixed

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,2 @@
 # Install pypcd
 pip install pypcd
-
-# Install python-pcl
-sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl -y
-sudo apt-get update -y
-sudo apt-get install libpcl-all -y
